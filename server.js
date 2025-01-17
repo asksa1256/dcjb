9e360abd1cc24193c0b486f46b876b43cc6c0c9b
asksa1256@gmail.com <asksa1256@gmail.com>
2025-01-17 23:45:28 +0900

    node.js 세팅

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 3300;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __dirname });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
