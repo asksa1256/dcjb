d022ca82f1a04154f92b3d95e8570c8c06aff8e9
asksa1256@gmail.com <asksa1256@gmail.com>
2025-01-18 15:25:47 +0900

    서버 연동 일시 해제

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 8080;
+// const express = require("express");
+// const app = express();
+// const port = process.env.PORT || 8080;
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.sendFile("index.html", { root: __dirname });
-});
-
-// app.get("/log", (req, res) => {
-//   res.send("Hello from the server!");
+// app.get("/", (req, res) => {
+//   res.sendFile("index.html", { root: __dirname });
 // });
 
-app.listen(port, () => {
-  console.log(`Server Listening on ${port}`);
-});
+// // app.get("/log", (req, res) => {
+// //   res.send("Hello from the server!");
+// // });
+
+// app.listen(port, () => {
+//   console.log(`Server Listening on ${port}`);
+// });
